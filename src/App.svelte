12ac8d81427ee1be039cccf6de25ab35e05a2d40
@chinsuko_chan <177090151+chinsuko-chan@users.noreply.github.com>
2024-08-03 13:51:49 +0900

    initial workflowcontents w/ collapsible inputs

--- src/App.svelte
+++ src/App.svelte
@@ -7,6 +7,7 @@
 
   import WorkflowSelector from "./components/WorkflowSelector.svelte"
   import WorkflowUploadButton from "./components/WorkflowUploadButton.svelte"
+  import WorkflowContents from "./components/WorkflowContents.svelte"
 </script>
 
 {#snippet sidebar()}
@@ -188,108 +189,6 @@
   </nav>
 {/snippet}
 
-{#snippet contents()}
-  <article class="mx-6 mb-16 xl:mr-2">
-  <ul class="timeline timeline-vertical timeline-snap-icon timeline-compact">
-    <li>
-      <div class="timeline-middle">
-        <button class="btn btn-outline btn-xs btn-circle">11</button>
-      </div>
-      <div class="timeline-end timeline-box">
-        <section>
-          <h2>
-            <a aria-hidden="true" tabindex="-1" href="#heading-1">
-              <span class="mr-1 opacity-20 hover:opacity-60 text-base font-bold inline-block align-middle relative -mt-1">#</span>
-            </a>
-            Load Vae
-          </h2>
-
-          <h3 class="font-bold">Inputs</h3>
-          <ul>
-            <li>hoes</li>
-          </ul>
-
-          <h3 class="font-bold">Outputs</h3>
-          <ul>
-            <li>money</li>
-          </ul>
-        </section>
-      </div>
-      <hr />
-    </li>
-    <li>
-      <hr />
-      <div class="timeline-middle">
-        <button class="btn btn-outline btn-xs btn-circle">2</button>
-      </div>
-      <div class="timeline-end timeline-box"><section>
-        <h2>
-          <a aria-hidden="true" tabindex="-1" href="#heading-1">
-            <span class="mr-1 opacity-20 hover:opacity-60 text-base font-bold inline-block align-middle relative -mt-1">#</span>
-          </a>
-          Load Clip
-        </h2>
-
-        <h3 class="font-bold">Inputs</h3>
-        <ul>
-          <li>hoes</li>
-        </ul>
-
-        <h3 class="font-bold">Outputs</h3>
-        <ul>
-          <li>money</li>
-        </ul>
-      </section></div>
-      <hr />
-    </li>
-    <li>
-      <hr />
-      <div class="timeline-middle">
-        <button class="btn btn-outline btn-xs btn-circle">3</button>
-      </div>
-      <div class="timeline-end timeline-box"><section>
-        <h2>
-          <a aria-hidden="true" tabindex="-1" href="#heading-1">
-            <span class="mr-1 opacity-20 hover:opacity-60 text-base font-bold inline-block align-middle relative -mt-1">#</span>
-          </a>
-          Load Checkpoint
-        </h2>
-      </section></div>
-      <hr />
-    </li>
-    <li>
-      <hr />
-      <div class="timeline-middle">
-        <button class="btn btn-outline btn-xs btn-circle">69</button>
-      </div>
-      <div class="timeline-end timeline-box"><section>
-        <h2>
-          <a aria-hidden="true" tabindex="-1" href="#heading-1">
-            <span class="mr-1 opacity-20 hover:opacity-60 text-base font-bold inline-block align-middle relative -mt-1">#</span>
-          </a>
-          I love you
-        </h2>
-      </section></div>
-      <hr />
-    </li>
-    <li>
-      <hr />
-      <div class="timeline-middle">
-        <button class="btn btn-outline btn-xs btn-circle">420</button>
-      </div>
-      <div class="timeline-end timeline-box"><section>
-        <h2>
-          <a aria-hidden="true" tabindex="-1" href="#heading-1">
-            <span class="mr-1 opacity-20 hover:opacity-60 text-base font-bold inline-block align-middle relative -mt-1">#</span>
-          </a>
-          There is no meme
-        </h2>
-      </section></div>
-    </li>
-  </ul>
-  </article>
-{/snippet}
-
 <div class="container mx-auto">
   <main class="bg-base-100 drawer lg:drawer-open">
     <input id={DRAWER_ID} type="checkbox" class="drawer-toggle" />
@@ -303,7 +202,7 @@
 
     <div class="drawer-content">
       {@render navbar()}
-      {@render contents()}
+      <WorkflowContents />
     </div>
   </main>
 </div>
---
+++ src/components/WorkflowContents.svelte
@@ -0,0 +1,89 @@
+<script>
+  import { connectWorkflowManager } from "../stores/workflowManager.svelte"
+
+  const manager = connectWorkflowManager()
+
+  /** @todo maybe something better than JSON.stringify */
+  function formatInputs(inputsObject) {
+    return Object.entries(inputsObject).map(([key, value]) => {
+      return [key, JSON.stringify(value)]
+    })
+  }
+
+  let nodeEntries = $derived(Object.entries(manager.current))
+
+  $inspect(manager.current)
+</script>
+
+{#snippet inputsListItem(key, displayValue)}
+  <li>
+    <details class="collapse">
+      <summary class="collapse-title p-2 min-h-8">
+        <code class="btn btn-xs rounded-xl dark:border-base-content">{key}</code>
+      </summary>
+      <div class="collapse-content">
+        <span>{displayValue}</span>
+      </div>
+    </details>
+  </li>
+{/snippet}
+
+{#snippet leadingHr(index)}
+  {#if index !== 0}
+    <hr />
+  {/if}
+{/snippet}
+
+{#snippet trailingHr(length, index)}
+  {#if length - 1 !== index}
+    <hr />
+  {/if}
+{/snippet}
+
+
+<article class="mx-6 mb-16 xl:mr-2">
+  <ul class="timeline timeline-vertical timeline-snap-icon timeline-compact">
+    {#each nodeEntries as [id, node], index}
+      <li>
+        {@render leadingHr(index)}
+        <div class="timeline-middle">
+          <button class="btn btn-outline btn-xs btn-circle">
+            {id}
+          </button>
+        </div>
+        <div class="timeline-end timeline-box">
+          <section>
+            <h2 id={`node-${id}`}>
+              <a aria-hidden="true" tabindex="-1" href={`#node-${id}`}>
+                <span
+                  class="mr-1 opacity-20 hover:opacity-60 text-base font-bold inline-block align-middle relative -mt-1"
+                  >#</span
+                >
+              </a>
+              {node._meta.title}
+            </h2>
+
+            <h3 class="font-bold">Inputs</h3>
+            <ul>
+              {#each formatInputs(node.inputs) as [key, val]}
+                {@render inputsListItem(key, val)}
+              {/each}
+            </ul>
+          </section>
+        </div>
+        {@render trailingHr(nodeEntries.length, index)}
+      </li>
+    {/each}
+  </ul>
+</article>
+
+<style lang="postcss">
+  /* is there better way...? */
+  details[open] {
+    summary {
+      code.btn {
+        @apply border-secondary text-secondary bg-transparent;
+      }
+    }
+  }
+</style>
