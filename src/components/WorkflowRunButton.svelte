6be41e1eabb83d32e45161d5ac4ef0cc0d45e120
@chinsuko_chan <177090151+chinsuko-chan@users.noreply.github.com>
2024-08-06 22:50:04 +0900

    side quest: button to commit changes then run workflow

--- src/components/WorkflowRunButton.svelte
+++ src/components/WorkflowRunButton.svelte
@@ -1,6 +1,11 @@
 <script>
   import { api, STATUS } from "../stores/apiConnectionManager.svelte"
   import { runner } from "../stores/workflowRunnerManager.svelte"
+  import { connectWorkflowManager } from "../stores/workflowManager.svelte"
+
+  import runAndGoSvg from "../assets/run-n-go.svg?raw"
+
+  const manager = connectWorkflowManager()
 
   let buttonMessage = $derived.by(() => {
     if (api.status === STATUS.IDLE) return "Run Workflow"
@@ -21,14 +26,33 @@
       console.error(e)
     }
   }
+
+  let mainButtonDisabled = $derived.by(() => {
+    if (api.status !== STATUS.IDLE) return true
+
+    return manager.hasUncommittedChanges
+  })
+
+  function commitAndRun() {
+    manager.commitChanges()
+    tryQueue()
+  }
 </script>
 
+{#if manager.hasUncommittedChanges}
+  <button
+    class="btn btn-ghost btn-circle border-0 mr-4"
+    title="Commit current changes and run the workflow."
+    onclick={commitAndRun}
+  >
+    {@html runAndGoSvg}
+  </button>
+{/if}
 <button
   class="btn btn-sm btn-outline"
   class:btn-success={api.status === STATUS.IDLE}
   class:btn-warning={api.status === STATUS.RUNNING}
-  class:btn-ghost={api.status !== STATUS.IDLE}
-  disabled={api.status !== STATUS.IDLE}
+  disabled={mainButtonDisabled}
   onclick={tryQueue}
 >
   {buttonMessage}
