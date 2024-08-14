f8c839c0ff74b33a85baf3b9466fa47a60ef358f
@chinsuko_chan <177090151+chinsuko-chan@users.noreply.github.com>
2024-08-14 23:45:51 +0900

    helper fn for getting and saving store values to localStorage

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
   plugins: [svelte(svelteConfig)],
   resolve: {
     alias: {
+      lib: path.resolve(__dirname, "./src/lib"),
       assets: path.resolve(__dirname, "./src/assets"),
       stores: path.resolve(__dirname, "./src/stores"),
       components: path.resolve(__dirname, "./src/components"),
