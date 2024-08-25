ef017f5e41d97724bf16adbfbd16efdaa5dcd11a
@chinsuko_chan <177090151+chinsuko-chan@users.noreply.github.com>
2024-08-25 22:00:18 +0900

    add resizable grid outputs for node images

--- src/components/EditPage/NodeOutput.svelte
+++ src/components/EditPage/NodeOutput.svelte
@@ -4,10 +4,21 @@
   /** support rendering images, or a stringified JSON */
   let { workflowName, id, key, value } = $props()
 
+  const KEY = "extraUI.components.editPage.nodeOutput.imagesLayout"
+  import connect from "lib/localStore"
+  const localViewState = connect(KEY, {})
+  let viewState = $state(localViewState.current)
+  let imagesLayout = $derived(viewState[id]?.[key] || 1)
+
   import { connectWorkflow } from "stores/workflows.svelte"
   let workflow = $derived(connectWorkflow(workflowName))
 
   import fetchImage from "stores/imageCache.svelte"
+
+  import svgCols1 from "assets/1x1.svg?raw"
+  import svgCols2 from "assets/2x2.svg?raw"
+  import svgCols3 from "assets/3x3.svg?raw"
+  import svgCols4 from "assets/4x4.svg?raw"
 
   async function getImageAttributes(attributes) {
     return await fetchImage(
@@ -29,22 +40,47 @@
     images = await Promise.all(value.map(getImageAttributes))
     untrack(() => images)
   })
+
+  function changeImagesLayout(newColCount) {
+    viewState[id] ||= {}
+    viewState[id][key] = newColCount
+    localViewState.save(viewState)
+  }
 </script>
 
 <li>
   <div class="flex flex-col gap-2 p-2">
-    <code class="badge badge-outline badge-primary">{key}</code>
-    <div>
-      {#if images.length}
+    {#if images.length}
+      <div class="flex justify-between">
+        <code class="badge badge-outline badge-primary">{key}</code>
+        <div>
+          {#each [[1, svgCols1], [2, svgCols2], [3, svgCols3], [4, svgCols4]] as [col, svgAsset]}
+            <button
+              class="btn btn-sm"
+              class:btn-ghost={col !== imagesLayout}
+              class:btn-outline={col === imagesLayout}
+              class:btn-primary={col === imagesLayout}
+              onclick={() => changeImagesLayout(col)}>{@html svgAsset}</button
+            >
+          {/each}
+        </div>
+      </div>
+      <div
+        class="grid"
+        class:grid-cols-2={imagesLayout === 2}
+        class:grid-cols-3={imagesLayout === 3}
+        class:grid-cols-4={imagesLayout === 4}
+      >
         {#each images as attributes}
           <img src={attributes.blob} alt={attributes.filename} />
         {/each}
-      {:else}
-        <!-- ambiguous element... curious... -->
-        <div class="bg-base-200 w-full px-4 py-2 rounded-md break-all">
-          <code class="text-sm">{value}</code>
-        </div>
-      {/if}
-    </div>
+      </div>
+    {:else}
+      <!-- ambiguous element... curious... -->
+      <code class="badge badge-outline badge-primary">{key}</code>
+      <div class="bg-base-200 w-full px-4 py-2 rounded-md break-all">
+        <code class="text-sm">{JSON.stringify(value)}</code>
+      </div>
+    {/if}
   </div>
 </li>
