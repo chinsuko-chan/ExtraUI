f21e48059d0989000884e0a1f5b1e8ec8b6d0a2c
@chinsuko_chan <177090151+chinsuko-chan@users.noreply.github.com>
2024-10-07 21:24:14 +0900

    make smalelre wen not usei many pins

--- src/components/PinnedInputs/Index.svelte
+++ src/components/PinnedInputs/Index.svelte
@@ -6,15 +6,25 @@
   let nodes = $derived.by(() => {
     return workflowPins.find(({ name }) => name === workflowName)?.nodes || []
   })
+
+  let maxWidthClasses = $derived.by(() => {
+    if (nodes.length === 2) return "lg:max-w-4xl"
+    if (nodes.length >= 3) return "lg:max-w-full"
+    return ""
+  })
+
+  let colClasses = $derived.by(() => {
+    if (nodes.length === 2) return "lg:grid-cols-2"
+    if (nodes.length >= 3) return "lg:grid-cols-2 2xl:grid-cols-3"
+    return ""
+  })
 </script>
 
 {#if nodes.length}
   <aside
-    class="md:ml-3 p-2 bg-base-200/80 backdrop-blur-lg shadow rounded z-30 max-w-xl lg:max-w-full sticky top-16"
+    class={`md:ml-3 p-2 bg-base-200/80 backdrop-blur-lg shadow rounded z-30 max-w-xl sticky top-16 ${maxWidthClasses}`}
   >
-    <div
-      class="grid grid-flow-dense grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3"
-    >
+    <div class={`grid grid-flow-dense grid-cols-1 ${colClasses}`}>
       {#each nodes as { id, keys }}
         <div class="grid grid-cols-[auto_1fr] gap-x-2 gap-y-1 mb-auto">
           {#each keys as { key }, keyIndex}
